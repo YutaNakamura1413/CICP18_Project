e93989d314a6b794f9eef2c236c38a19415c9d22
Yuta Nakamura <nakamuta.yuta.ns1@is.naist.jp>
2018-09-19 10:23:01 +0900

    2018/9/19

--- SmartLock/src/js/callKeyContract.js
+++ SmartLock/src/js/callKeyContract.js
@@ -1,3 +1,29 @@
+// ボタンを押した時の処理
+function OnButtonClick() {
+  // 必要なパッケージをインポート
+  const fs = require("fs");
+  const Web3 = require('web3');
+
+  // web3の初期化
+  web3 = new Web3();
+  // プライベートネットワークと接続
+  if (!web3.currentProvider) {
+    web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
+  }
+
+  //simplestorageのABI
+  var contract = require('../../build/contracts/KeyContract.json');
+  var ABI = contract.abi;
+  var address = contract.networks[13].address;
+  var smartKey = web3.eth.contract(ABI).at(address);
+  smartKey.close({from:web3.eth.accounts[0]});
+
+
+
+  target = document.getElementById("output");
+  target.innerHTML = "鍵が空きました";
+}
+//
 // 必要なパッケージをインポート
 const fs = require("fs");
 const Web3 = require('web3');
@@ -6,25 +32,12 @@ const Web3 = require('web3');
 web3 = new Web3();
 // プライベートネットワークと接続
 if (!web3.currentProvider) {
-    web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
+  web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
 }
 
-// ボタンを押した時の処理
-function OnButtonClick() {
-
-    target = document.getElementById("output");
-    target.innerHTML = "鍵が空きました";
-}
 //simplestorageのABI
 var contract = require('../../build/contracts/KeyContract.json');
 var ABI = contract.abi;
-// console.log(ABI);
-
-//デプロイしたアドレス
 var address = contract.networks[13].address;
-// console.log(address);
-//
 var smartKey = web3.eth.contract(ABI).at(address);
-// console.log(smartKey);
-
 smartKey.close({from:web3.eth.accounts[0]});
