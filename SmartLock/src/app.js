10ded65011ef81748b86a43f5771757bc6e4e30c
masanariyamamoto <masa.060419@gmail.com>
2018-12-02 22:48:00 +0900

    add user account

--- SmartLock/src/app.js
+++ SmartLock/src/app.js
@@ -1,4 +1,3 @@
-//Expressのモジュールを取り込んで生成
 const express = require('express')
 const app = express()
 const portNo = 3000
@@ -7,69 +6,31 @@ const Web3 = require('web3');
 require('dotenv').config();
 const env = process.env.AUTH_KEY;
 const request = require('request');
-const url = 'https://api.candyhouse.co/public/sesame/fc5bad76-b2f0-4876-b4ea-17e3bc93fd05';
+const bodyParser = require('body-parser')
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: true}));
 // get the key status
-const getStatusOptions = {
-  url: url,
-  headers: {
-    'Authorization': env
-  }
-};
+// const getStatusOptions = {
+//   url: url,
+//   headers: {
+//     'Authorization': env
+//   }
+// };
 
 //鍵を閉める時のリクエスト情報
 const postLockOptions = {
-  url: url,
+  url: "https://51b0fb00.ngrok.io/close",
   method: 'POST',
-  headers: {
-    'Authorization': env,
-    'content-type': 'application/json'
-  },
-  body: JSON.stringify({
-    "command": "lock"
-  })
 };
 
 //鍵を開ける時のリクエスト情報
 const postUnlockOptions = {
-  url: url,
+  url: "https://51b0fb00.ngrok.io/open",
   method: 'POST',
-  headers: {
-    'Authorization': env,
-    'content-type': 'application/json'
-  },
-  body: JSON.stringify({
-    "command": "unlock"
-  })
 };
-
-const callApi = (options) => {
-  return new Promise((resolve, reject) => {
-    request(options, (error, response, body) => {
-      let json = JSON.parse(body);
-      if (!error && response.statusCode == 200) {
-        resolve(json);
-      } else {
-        reject(json.message);
-      }
-    });
-  });
-}
-
-const callApiResult = (options) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      request(options, (error, response, body) => {
-        let json = JSON.parse(body);
-        if (!error && response.statusCode == 200) {
-          resolve(json);
-        } else {
-          reject(json.message);
-        }
-      });
-    }, 10000);
-  });
-}
 
 // web3の初期化
 web3 = new Web3();
@@ -83,10 +44,8 @@ var contract = require('../build/contracts/KeyContract.json');
 var ABI = contract.abi;
 var address = contract.networks[13].address;
 var smartKey = web3.eth.contract(ABI).at(address);
-var status = smartKey.getStatus.call({from:web3.eth.accounts[0]});
+var status
 var event = smartKey.OpenClose();
-// console.log(event);
-
 //イベント監視
 event.watch(function (error, result) {
  console.log('watching "OpenClose" event!');
@@ -94,35 +53,24 @@ event.watch(function (error, result) {
     console.log(result);
 });
 
-app.set('view engine', 'ejs');
-
 app.get('/', (req, res, next) => {
   res.render("index")
 })
 
 app.post('/open', (req, res, next) => {
-  //   target = document.getElementById("output");
-  //   target.innerHTML = "鍵が空きました";})
-  callApi(getStatusOptions).then((res) => {
-    if(res.locked == true && status == true) {
-      console.log('status OK');
-      return callApi(postUnlockOptions);
-    }
-  }).then((res) => {
-    console.log(res);
-    return callApiResult({
-      url: `https://api.candyhouse.co/public/action-result?task_id=${res.task_id}`,
-      headers: {
-        'Authorization': env
-      }
+  console.log(req.body["user_account"])
+    //smartKey.open({from:web3.eth.accounts[0]});
+})
+
+app.post('/close', (req, res, next) => {
+  if (status == false) {
+    request(postLockOptions, (error, response, body) => {
+      // let json = JSON.parse(body);
+      // if (json == "closed") {
+    smartKey.close({from:web3.eth.accounts[0]});
+      // }
     });
-  }).then((res) => {
-    console.log(res);
-    if(res.successful == true) {
-      console.log(res.successful);
-      smartKey.open({from:web3.eth.accounts[0]});
-    }
-  });
+  }
 })
 
 app.listen(portNo, () => {
